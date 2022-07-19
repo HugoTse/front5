79dab784def13b6b59bf257686a13496e8a8ac8d
Hugo Tse <hugotse8@gmail.com>
2022-07-18 23:18:20 -0700

    updates to frontend

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,11 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "storage": {
+        "s3a06357d6": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import "@aws-amplify/ui-react/styles.css";
 
 import {
@@ -9,8 +8,23 @@ import {
   View,
   Card,
 } from "@aws-amplify/ui-react";
-import { Amplify, API } from 'aws-amplify';
 
+import { Amplify, API, Storage } from 'aws-amplify';
+import React, { useState, useEffect } from "react";
+// Table stuff
+import { Flex, 
+         Table, 
+         TableCell, 
+         TableHead,
+         TableRow,
+         TableBody,
+         ThemeProvider,
+         Theme,
+         TextAreaField } from '@aws-amplify/ui-react';
+import { AiTwotoneDelete } from "react-icons/ai";
+
+
+// For API
 Amplify.configure({
   API: {
     endpoints: [
@@ -27,46 +41,388 @@ Amplify.configure({
 })
 
 function App({ signOut }) {
+  // Table stuff
+  const theme: Theme = {
+    name: 'table-theme',
+    tokens: {
+      components: {
+        table: {
+          row: {
+            hover: {
+              backgroundColor: { value: '{colors.blue.20}' },
+            },
+  
+            striped: {
+              backgroundColor: { value: '{colors.blue.10}' },
+            },
+          },
+  
+          header: {
+            color: { value: '{colors.blue.80}' },
+            fontSize: { value: '{fontSizes.xl}' },
+          },
+  
+          data: {
+            fontWeight: { value: '{fontWeights.semibold}' },
+          },
+        },
+      },
+    },
+  };
 
-const apiName = 'testing';
-const path = 'http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=json'; 
-const myInit = { // OPTIONAL
-headers: {}, // OPTIONAL
-response: true, // OPTIONAL (return the entire Axios response object instead of only response.data)
-// queryStringParameters: {  // OPTIONAL
-//     name: 'param',
-//   },
-};
+  const[cid, setCid] = useState('');
+  const[timestamp, setTimestamp] = useState('');
+  const[thand, setThand] = useState('');
+  const[fname, setFname] = useState('');
+  const[lname, setLname] = useState('');
+  const[dob, setDob] = useState('');
+  const[region, setRegion] = useState('');
+  const[text, setText] = useState('');
+  const[ir, setIr] = useState('');
 
-async function callAPI() { 
-    // const apiName = 'uploadImage';
-    // const path = '20220714s3bucket/test3.jpeg';
-    // const myInit = { // OPTIONAL
-    //     body: {}, // replace this with attributes you need
-    //     headers: {}, // OPTIONAL
-    // };
-    // return await API.post(apiName, path, myInit);
-    console.log('Invoked callAPI function');
-    API
-    .get(apiName, path, myInit)
-    .then(response => {
-      console.log(response);
-    })
-    .catch(error => {
-      console.log(error.response);
-    });
-}
+  // For calling the API
+  const apiName = 'testing';
+  const path = 'http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=json'; 
+  const myInit = { // OPTIONAL
+  headers: {}, // OPTIONAL
+  response: true, // OPTIONAL (return the entire Axios response object instead of only response.data)
+  // queryStringParameters: {  // OPTIONAL
+  //     name: 'param',
+  //   },
+  };
 
+  // For calling the API
+  async function callAPI() { 
+      // const apiName = 'uploadImage';
+      // const path = '20220714s3bucket/test3.jpeg';
+      // const myInit = { // OPTIONAL
+      //     body: {}, // replace this with attributes you need
+      //     headers: {}, // OPTIONAL
+      // };
+      // return await API.post(apiName, path, myInit);
+      console.log('Invoked callAPI function');
+      API
+      .get(apiName, path, myInit)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error.response);
+      });
+  }
+
+  // For uploadImage
+  async function onChange(e) {
+    e.preventDefault();
+    // If there are no files, return
+    if (!e.target.files[0]) return
+    // Set the variable file to the uploaded file
+    const file = e.target.files[0];
+    // Set the key for the file
+    const imageKey = 'image/raw/' + file.name;
+    setIr(imageKey);
+    // Log the key
+    console.log(imageKey);
+    try {
+      // Place the image in the bucket
+      await Storage.put(imageKey, file, {
+        contentType: "image/png", // contentType is optional
+      })
+      // This returns the key
+      .then((res) => 
+        console.log(res)
+      );
+    } catch (error) {
+      console.log("Error uploading file: ", error);
+    }
+  }
+
+  // For submitComment or processImage
+  async function Comment(e) {
+    e.preventDefault();
+    console.log(cid,
+                timestamp,
+                thand,
+                fname,
+                lname,
+                dob,
+                region,
+                text,
+                ir);
+    if(ir == ''){
+      // No image uploaded
+      console.log('No image uploaded, so call submitComment');
+      const objectKey = 'input/'+cid+'_'+timestamp+'_'+thand;
+      const comment = {
+        "userMetaData": {
+          "campaign_id": cid,
+          "timestamp": timestamp,
+          "twitter_handle": thand,
+          "first_name": fname,
+          "last_name": lname,
+          "date_of_birth": dob,
+          "region": region
+        },
+        "data": {
+          "text": text
+        }
+      }
+      try {
+        // Place the image in the bucket
+        await Storage.put(objectKey, comment, {
+          contentType: "application/json", // contentType is optional
+        })
+        // This returns the key
+        .then((res) => 
+          console.log(res)
+        );
+      } catch (error) {
+        console.log("Error uploading file: ", error);
+      }
+    } else {
+      // Image was uploaded
+      console.log('Image was uploaded');
+      const objectKey = 'input/'+cid+'_'+timestamp+'_'+thand;
+      const comment = {
+        "userMetaData": {
+          "campaign_id": cid,
+          "timestamp": timestamp,
+          "twitter_handle": thand,
+          "first_name": fname,
+          "last_name": lname,
+          "date_of_birth": dob,
+          "region": region
+        },
+        "data": {
+          "text": text,
+          "image_reference": ir
+        }
+      }
+      try {
+        // Place the image in the bucket
+        await Storage.put(objectKey, comment, {
+          contentType: "application/json", // contentType is optional
+        })
+        // This returns the key
+        .then((res) => 
+          console.log(res)
+        );
+      } catch (error) {
+        console.log("Error uploading file: ", error);
+      }
+    }
+    // Reset the variables
+    setCid('');
+    setTimestamp('');
+    setThand('');
+    setFname('');
+    setLname('');
+    setDob('');
+    setRegion('');
+    setText('');
+    setIr('');
+  }
 
   return (
     <View className="App">
       <Card>
         {/* <Image src={logo} className="App-logo" alt="logo" /> */}
-        <Heading level={1}>We now have Auth!</Heading>
+        <Heading level={1}>Nautilus Marketing</Heading>
       </Card>
-      <Button onClick={signOut}>Sign Out</Button>
 
-      <Button onClick={() => callAPI()}> POSTING </Button>
+      <div className='formDiv'>
+        <Button onClick={signOut}>Sign Out</Button>
+      </div>
+
+      {/* Form onsubmit = handlesubmit */}
+      {/* Upload Image */}
+      <div className='formDiv'>
+        <input
+          type="file"
+          onChange={onChange}
+
+        />
+      </div>
+
+      {/* Inputs */}
+      <h2>Comment Metadata</h2>
+      <form onSubmit={Comment}>
+        <div className='formDiv'>
+          <input
+            className='input'
+            // onChange={e => setFormData({ ...formData, 'cid': e.target.value})}
+            onChange={e => setCid(e.target.value)}
+            placeholder="Campaign ID"
+            value={cid}
+          />
+        </div>
+        <div className='formDiv'>
+          <input
+            className='input'
+            onChange={e => setTimestamp(e.target.value)}
+            placeholder="Timestamp"
+            value={timestamp}
+          />
+        </div>
+        <div className='formDiv'>
+          <input
+            className='input'
+            onChange={e => setThand(e.target.value)}
+            placeholder="Twitter Handle"
+            value={thand}
+          />
+        </div>
+        <div className='formDiv'>
+          <input
+            className='input'
+            onChange={e => setFname(e.target.value)}
+            placeholder="First Name"
+            value={fname}
+          />
+        </div>
+        <div className='formDiv'>
+          <input
+            className='input'
+            onChange={e => setLname(e.target.value)}
+            placeholder="Last Name"
+            value={lname}
+          />
+        </div>
+        <div className='formDiv'>
+          <input
+            className='input'
+            onChange={e => setDob(e.target.value)}
+            placeholder="Date of Birth"
+            value={dob}
+          />
+        </div>
+        <div className='formDiv'>
+          <input
+            className='input'
+            onChange={e => setRegion(e.target.value)}
+            placeholder="Region"
+            value={region}
+          />
+        </div>
+
+        <h2>Comment</h2>
+        <div className='formDiv'>
+          <textarea
+            className='input'
+            onChange={e => setText(e.target.value)}
+            placeholder="Text"
+            value={text}
+          />
+        </div>
+        <div className='formDiv'>
+          <button type='submit' className='submitButton'>SUBMIT</button>
+        </div>
+      </form>
+      {/* <div className='formDiv'>
+        <input
+          className='input'
+          onChange={e => setFormData({ ...formData, 'text': e.target.value})}
+          placeholder="Text"
+          value={formData.text}
+        />
+      </div> */}
+
+      <div className='testDiv'>
+        <Button onClick={() => callAPI()}> Post to API GW connected to Lambda </Button>
+      </div>
+      <div className='testDiv'>
+        <Button onClick={() => callAPI()}> Post to API GW connected to S3 </Button>
+      </div>
+
+      <div className='tableDiv'>
+      {/* Table stuff */}
+      <Flex direction="column">
+      <Table variation="bordered">
+        <TableRow>
+          <TableCell>Bordered</TableCell>
+          <TableCell>Bordered</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>Bordered</TableCell>
+          <TableCell>Bordered</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>Bordered</TableCell>
+          <TableCell>Bordered</TableCell>
+        </TableRow>
+      </Table>
+      </Flex>
+      </div>
+      <div className='tableDiv'>
+      <ThemeProvider theme={theme} colorMode="light">
+        <Table highlightOnHover variation="striped">
+          <TableHead>
+            <TableRow>
+              <TableCell>Customer, SA, Gap</TableCell>
+              <TableCell>Service</TableCell>
+              <TableCell>GCP Claim/Customer Feedback</TableCell>
+              <TableCell>Win/Loss to GCP? Key factor resulting in loss and learnings</TableCell>
+              <TableCell>Priority/AWS GCP Compete Team Response</TableCell>
+              <TableCell>Service Team PFR / Roadmap</TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          <TableRow>
+              <TableCell>
+                <TextAreaField label="" defaultValue="Amplify UI is awesome!" />
+              </TableCell>
+              <TableCell>
+                <TextAreaField label="" defaultValue="Amplify UI is awesome!" />
+              </TableCell>
+              <TableCell>
+                <TextAreaField label="" defaultValue="Amplify UI is awesome!" />
+              </TableCell>
+              <TableCell>
+                <TextAreaField label="" defaultValue="Amplify UI is awesome!" />
+              </TableCell>
+              <TableCell>
+                <TextAreaField label="" defaultValue="Amplify UI is awesome!" />
+              </TableCell>
+              <TableCell>
+                <TextAreaField label="" defaultValue="Amplify UI is awesome!" />
+              </TableCell>
+              <TableCell>
+                <Button
+                  loadingText=""
+                  onClick={() => alert('hello')}
+                  ariaLabel=""
+                >
+                  Submit
+                </Button>
+            </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell>abc</TableCell>
+              <TableCell>abc</TableCell>
+              <TableCell>abc</TableCell>
+              <TableCell>abc</TableCell>
+              <TableCell>abc</TableCell>
+              <TableCell>abc</TableCell>
+              <TableCell>
+                <Button
+                  loadingText=""
+                  onClick={() => alert('hello')}
+                  ariaLabel=""
+                >
+                  Edit
+                </Button>
+              
+                <AiTwotoneDelete className='deleteIcon'/>
+            </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </ThemeProvider>
+      </div>
+      
+
     </View>
   );
 }
